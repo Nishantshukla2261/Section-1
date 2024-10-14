fade16c6506306dbe39fdd92809862e91edc2e35
Nishantshukla2261 <nishantshukla2261@gmail.com>
2024-10-14 17:23:18 +0530

    condition + index

---
+++ Javascript/conditions.js
@@ -0,0 +1,41 @@
+num = 4;
+if (num % 2 == 0) {
+    console.log('even');
+
+} else {
+    console.log('odd');
+
+}
+
+// wap to check if person is eligible or not
+
+age = 12;
+if (age >= 18) {
+    console.log('Eligible for DL');
+
+} else {
+    console.log('Not Eligible for DL');
+
+}
+
+age = 20;
+if (age >= 18) {
+    var abc = 'something'
+    let a = 'local'
+    console.log(a);
+    const pi =3.14;
+    // pi=3.15; constant means change nhi kr sakte 
+    
+    
+    console.log('Eligible for DL');
+
+} else {
+    console.log('Not Eligible for DL');
+
+}
+console.log(abc);
+// console.log(a); yha pe ye line error degi
+// /let or const (constant) ka use karne hai hm var ka use itna nhi karte orr by default ye global variable hi hote hai
+//  node --watch conditions
+
+
--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,29 @@
 console.log('Hello JavaScript');
+a=234
+b=16
+console.log(a+b);
+
+myname ='Nishant';
+console.log(myname);
+price=34.23;
+
+console.log(typeof myname);
+console.log(typeof a);
+console.log(typeof price);
+// typeof se data type pta karte hai
+console.log(1+1);
+console.log('1'+'1'+'abc');
+console.log('1'+1);
+console.log('1'+true);
+console.log('1'*1+2);
+
+console.log(1==1);
+console.log('1'==1);
+console.log('1'===1);
+console.log('2'===1);
+console.log('2'==2);
+// === mainly lgate hai yhi use karna chahiye
+
+
+
+
